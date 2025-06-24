ab535a06324c84170e9863d17d3943e6516e244c
Maria Annikova <mariaannikova@MacBook-Pro-Maria.local>
2025-06-24 14:15:51 +0300

    HW 9.1 Objects in JS

---
+++ hw9.1_objects/9.1_task1.js
@@ -0,0 +1,9 @@
+
+let student = {};
+
+student.firstName = "Марія";
+student.lastName = "Аннікова";
+student.age = 32;
+student.courses = ["Математика", "Історія", "Програмування"];
+
+console.log(JSON.stringify(student, null, 2));
---
+++ hw9.1_objects/9.1_task2.js
@@ -0,0 +1,10 @@
+const book = {
+    title: "Гобіт, або туди і звідти",
+    author: "Дж.Р.Р. Толкін",
+    year: "Ігор"
+};
+
+const {title, author} = book;
+
+console.log ("Назва книги: ", title); 
+console.log ("Автор: ", author);
